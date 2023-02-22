f54d904b262b27384fa8dbaf88a46b5fdc7ffe22
Hristijan Stavrov <hristijan.stavrov@gmail.com>
2023-02-22 20:12:39 +0100

    update

--- calculator_testing.cpp
+++ calculator_testing.cpp
@@ -24,6 +24,10 @@ int main() {
 	int int5 = y / 10;
 	int int6 = x % 10;
 	int int7 = y % 10;
+	int int8 = x / 100;
+	int int9 = y / 100;
+	int int10 = x % 100;
+	int int11 = y % 100;
 
 	cout << "all float:           " << float1 << endl;
 	cout << "all float %.20f:     " << fixed << setprecision(20) << float1 << endl;
@@ -36,6 +40,10 @@ int main() {
 	cout << "remove last digit y: " << int5 << endl;
 	cout << "get last digit x:    " << int6 << endl;
 	cout << "get last digit y:    " << int7 << endl;
+	cout << "remove 2last digit x: " << int8 << endl;
+	cout << "remove 2last digit y: " << int9 << endl;
+	cout << "get 2last digit x:    " << int10 << endl;
+	cout << "get 2last digit y:    " << int11 << endl;
 
 	return 0;
 }
