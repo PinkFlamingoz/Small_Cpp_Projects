732f158aa654f9347d03ee59bb27f66c720ce1b5
Hristijan Stavrov <hristijan.stavrov@gmail.com>
2023-02-26 15:33:13 +0100

    update

---
+++ strong_password.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<limits>
+
+using namespace std;
+
+string get_user_password();
+bool check_password(string password);
+void print_error(bool uppercase, bool lowercase, bool number, bool symbol, bool length);
+
+int main() {
+	// Get password input
+	// Check for password validation
+	// Print results
+	string password = get_user_password();
+}
+
+template <typename T>
+T get_valid_input(const string& prompt) {
+	T input;
+	while (true) {
+		cout << prompt;
+		if (cin >> input) {
+			// The user entered a valid number
+			break;
+		}
+		else {
+			// The user did not enter a valid number
+			cout << "Error: Please enter a valid input." << endl;
+			cin.clear(); // Clear the error state
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard any input in the buffer
+		}
+	}
+	return input;
+}
+
+string get_user_password() {
+	string password;
+	do {
+		password = get_valid_input<string>("Enter password: ");
+	} while (!check_password(password));
+	return password;
+}
+
+bool check_password(string password) {
+	bool uppercase = false;
+	bool lowercase = false;
+	bool number = false;
+	bool symbol = false;
+	bool length = false;
+	bool valid = false;
+
+	int password_length = password.length();
+	for (int i = 0; i < password_length; i++) {
+		if (isupper(password[i])) {
+			uppercase = true;
+		}
+		if (islower(password[i])) {
+			lowercase = true;
+		}
+		if (isdigit(password[i])) {
+			number = true;
+		}
+		if (!isalnum(password[i])) {
+			symbol = true;
+		}
+		if (password_length >= 8) {
+			length = true;
+		}
+	}
+	print_error(uppercase, lowercase, number, symbol, length);
+	if (uppercase && lowercase && number && symbol && length) {
+		valid = true;
+		cout << "Password is valid!" << endl;
+	}
+
+	return valid;
+}
+
+void print_error(bool uppercase, bool lowercase, bool number, bool symbol, bool length) {
+	if (!uppercase) {
+		cout << "Password needs one upper case!" << endl;
+	}
+	if (!lowercase) {
+		cout << "Password needs one lower case!" << endl;
+	}
+	if (!number) {
+		cout << "Password needs one number!" << endl;
+	}
+	if (!symbol) {
+		cout << "Password needs one symbol!" << endl;
+	}
+	if (!length) {
+		cout << "Password needs to be at least 8 characters!" << endl;
+	}
+}
---
+++ swap_characters.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<limits>
+
+using namespace std;
+
+string correct_text(string text);
+void print_result(string result);
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "Error 1: Enter only one argument! " << endl;
+		return 1;
+	}
+	else {
+		// Check for vowels and replace them
+		string  result = correct_text(argv[1]);
+		// Print result
+		print_result(result);
+
+		return 0;
+	}
+}
+
+string correct_text(string text) {
+	int length = text.length();
+	for (int i = 0; i < length; i++)
+		switch (text[i]) {
+		case 'a':
+			text[i] = '6';
+			break;
+		case 'e':
+			text[i] = '3';
+			break;
+		case 'i':
+			text[i] = '1';
+			break;
+		case 'o':
+			text[i] = '0';
+			break;
+		}
+
+	return text;
+}
+
+void print_result(string result) {
+	cout << result << endl;
+}
