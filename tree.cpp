a0b4aea1b304c4238321d46762a2e27ee9a520b9
Hristijan Stavrov <xixistavrov@gmail.com>
2023-05-24 10:44:15 +0200

    update

--- tree.cpp
+++ tree.cpp
@@ -78,7 +78,11 @@ using namespace std;
 // AVL Tree
 // AVL tree is a self-balancing Binary Search Tree (BST) where the difference between heights of left and right subtrees cannot be more than one(1) for all nodes. 
 // This difference is called the Balance Factor. BF = ( LEFT SUB TREE HEIGHT - RIGHT SUB TREE HEIGHT ) == { -1, 0, 1}
-//
+// 
+// NOTE: We always start by calculating the BF for all nodes and then we start balancing on the lowest level imbalanced node, after each operation we check the BF again for each node.
+// When we balance a tree, we always consider three nodes from the node where the imbalance is happening, and if we have more nodes with imbalance we first balance the node at the lowest level.
+// Then we recalculate the balance for every node, usually the other nodes sometimes balance out, that's why we balance the lowest level imbalanced node first.
+// 
 // To maintain the balance in AVL tree, we perform Rotations. Depending on different imbalance cases, we have 4 basic types of rotations –
 // LEFT LEFT Imbalance/case ---> (RIGHT Rotation)
 //		 z                     y      
